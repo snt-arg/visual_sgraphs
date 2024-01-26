680a53c4e7ebbd0bf2c77646ba9e2cf6936e1cd3
Ali Tourani <a.tourani1991@gmail.com>
2024-01-26 17:51:32 +0100

    [fix] - update Run Geo/Sem threads

--- orb_slam3/src/GeometricSegmentation.cc
+++ orb_slam3/src/GeometricSegmentation.cc
@@ -4,5 +4,14 @@ namespace ORB_SLAM3
 {
     GeometricSegmentation::GeometricSegmentation(Atlas *pAtlas) {}
 
-    void GeometricSegmentation::Run() {}
+    void GeometricSegmentation::Run()
+    {
+        while (1)
+        {
+            // Check if there are new keyframes in the buffer
+            // Apply RANSAC
+            // Add the new planes to the KF
+            usleep(3000);
+        }
+    }
 }
--- orb_slam3/src/SemanticSegmentation.cc
+++ orb_slam3/src/SemanticSegmentation.cc
@@ -4,5 +4,11 @@ namespace ORB_SLAM3
 {
     SemanticSegmentation::SemanticSegmentation(Atlas *pAtlas) {}
 
-    void SemanticSegmentation::Run() {}
+    void SemanticSegmentation::Run()
+    {
+        while (1)
+        {
+            usleep(3000);
+        }
+    }
 }
