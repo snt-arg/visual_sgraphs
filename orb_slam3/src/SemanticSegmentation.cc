a29edfed8af69265457421dd41f0846cfe5d9532
Ali Tourani <a.tourani1991@gmail.com>
2024-02-05 10:10:26 +0100

    [feat] - add TODOs for Semantic Segmentation

--- orb_slam3/src/SemanticSegmentation.cc
+++ orb_slam3/src/SemanticSegmentation.cc
@@ -8,6 +8,13 @@ namespace ORB_SLAM3
     {
         while (1)
         {
+            // Get the unfiltered point cloud of the KF
+            // Get the probability map for the pointcloud from Semantic Segmenter
+            // For probabilities > 0.9, as the wall, segment all the points in the point cloud
+            // Return the pointcloud with only walls and floors
+            // Perform distance filter and downsample
+            // Send it to the Ransac and get the plane equations
+            // Do the association with the planes in Geometric
             usleep(3000);
         }
     }
