2f08abd699a0c97b4cd4582f7fe638693e66bb94
richarJ2002 <jason2richards@gmail.com>
2026-06-25 20:12:04 +0200

    Adding doxygen and formatting

--- core/include/SemanticSegmentation.h
+++ core/include/SemanticSegmentation.h
@@ -2,95 +2,140 @@
  * This file is part of Visual S-Graphs (vS-Graphs).
  * Copyright (C) 2023-2025 SnT, University of Luxembourg
  *
- * 📝 Authors: Ali Tourani, Saad Ejaz, Hriday Bavle, Jose Luis Sanchez-Lopez, and Holger Voos
+ * 📝 Authors: Ali Tourani, Saad Ejaz, Hriday Bavle, Jose Luis Sanchez-Lopez,
+ * and Holger Voos
  *
- * vS-Graphs is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * vS-Graphs is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details: https://www.gnu.org/licenses/
-*/
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details: https://www.gnu.org/licenses/
+ */
 
 #ifndef SEMANTICSEG_H
 #define SEMANTICSEG_H
 
 #include "Atlas.h"
-#include "Utils.h"
 #include "GeoSemHelpers.h"
+#include "Utils.h"
 
-#include <unordered_map>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/common/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <unordered_map>
 
 namespace ORB_SLAM3
 {
-    class Atlas;
+class Atlas;
 
-    class SemanticSegmentation
-    {
-    private:
-        bool mGeoRuns;
-        Atlas *mpAtlas;
-        std::mutex mMutexNewKFs;
-        const uint8_t bytesPerClassProb = 4; // Four bytes per class probability - refer to scene_segment_ros
-        unsigned long int mLastProcessedKeyFrameId = 0;
-        std::list<std::tuple<uint64_t, cv::Mat, pcl::PCLPointCloud2::Ptr>> segmentedImageBuffer;
+class SemanticSegmentation
+{
+  private:
+    bool mGeoRuns;
 
-        // System parameters
-        SystemParams *sysParams;
+    Atlas *mpAtlas;
 
-    public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-        SemanticSegmentation(Atlas *pAtlas);
+    std::mutex mMutexNewKFs;
 
-        // Semantic segmentation frame buffer processing
-        std::list<std::tuple<uint64_t, cv::Mat, pcl::PCLPointCloud2::Ptr>> GetSegmentedFrameBuffer();
-        void AddSegmentedFrameToBuffer(std::tuple<uint64_t, cv::Mat, pcl::PCLPointCloud2::Ptr> *tuple);
+    // Four bytes per class probability - refer to scene_segment_ros
+    const uint8_t bytesPerClassProb = 4;
 
-        /**
-         * @brief Segments the point cloud into class specific point clouds and enriches them with the current keyframe point cloud
-         * @param pclPc2SegPrb the point cloud to be segmented
-         * @param segImgUncertainity the segmentation image uncertainty
-         * @param clsCloudPtrs the class specific point clouds
-         * @param thisKFPointCloud the current keyframe point cloud
-         */
-        void threshSeparatePointCloud(pcl::PCLPointCloud2::Ptr pclPc2SegPrb,
-                                      cv::Mat &segImgUncertainity,
-                                      std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> &clsCloudPtrs,
-                                      const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &thisKFPointCloud);
+    unsigned long int mLastProcessedKeyFrameId = 0;
 
-        /**
-         * @brief Gets all planes for each class specific point cloud using RANSAC
-         * @param clsCloudPtrs the class specific point clouds
-         * @param minCloudSize the minimum size of the point cloud to be segmented
-         * @return a vector of vector of point clouds
-         */
-        std::vector<std::vector<std::pair<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr, Eigen::Vector4d>>> getPlanesFromClassClouds(
+    std::list<std::tuple<uint64_t, cv::Mat, pcl::PCLPointCloud2::Ptr>>
+        segmentedImageBuffer;
+
+    // System parameters
+    SystemParams *sysParams;
+
+  public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    SemanticSegmentation(Atlas *pAtlas);
+
+    // Semantic segmentation frame buffer processing
+    std::list<std::tuple<uint64_t, cv::Mat, pcl::PCLPointCloud2::Ptr>>
+        GetSegmentedFrameBuffer();
+
+    void AddSegmentedFrameToBuffer(
+        std::tuple<uint64_t, cv::Mat, pcl::PCLPointCloud2::Ptr> *tuple);
+
+    /**
+     * @brief       Segments the point cloud into class specific point clouds
+     *              and enriches them with the current keyframe point cloud
+     *
+     * @param       pclPc2SegPrb
+     *              the point cloud to be segmented
+     *
+     * @param       segImgUncertainity
+     *              the segmentation image uncertainty
+     *
+     * @param       clsCloudPtrs
+     *              the class specific point clouds
+     *
+     * @param       thisKFPointCloud
+     *              the current keyframe point cloud
+     */
+    void threshSeparatePointCloud(
+        pcl::PCLPointCloud2::Ptr pclPc2SegPrb,
+        cv::Mat                 &segImgUncertainity,
+        std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> &clsCloudPtrs,
+        const pcl::PointCloud<pcl::PointXYZRGB>::Ptr         &thisKFPointCloud);
+
+    /**
+     * @brief       Gets all planes for each class specific point cloud using
+     *              RANSAC
+     *
+     * @param       clsCloudPtrs
+     *              the class specific point clouds
+     *
+     * @param       minCloudSize
+     *              the minimum size of the point cloud to be segmented
+     *
+     * @return      a vector of vector of point clouds
+     */
+    std::vector<std::vector<
+        std::pair<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr, Eigen::Vector4d>>>
+        getPlanesFromClassClouds(
             std::vector<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr> &clsCloudPtrs);
 
-        /**
-         * @brief Adds the planes to the Atlas
-         * @param clsPlanes the planes to be added
-         * @param clsConfs the confidence of the class predictions
-         */
-        void updatePlaneData(KeyFrame *pKF,
-                             std::vector<std::vector<std::pair<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr, Eigen::Vector4d>>> &clsPlanes);
+    /**
+     * @brief       Adds the planes to the Atlas
+     *
+     * @param       clsPlanes
+     *              the planes to be added
+     *
+     * @param       clsConfs
+     *              the confidence of the class predictions
+     */
+    void updatePlaneData(
+        KeyFrame *pKF,
+        std::vector<
+            std::vector<std::pair<pcl::PointCloud<pcl::PointXYZRGBA>::Ptr,
+                                  Eigen::Vector4d>>> &clsPlanes);
 
-        /**
-         * @brief Updates the map plane
-         * @param planeId the plane id
-         * @param clsId the class id
-         * @param confidence the confidence of the class predictions
-         */
-        void updatePlaneSemantics(int planeId, int clsId, double confidence);
+    /**
+     * @brief       Updates the map plane
+     *
+     * @param       planeId
+     *              the plane id
+     *
+     * @param       clsId
+     *              the class id
+     *
+     * @param       confidence
+     *              the confidence of the class predictions
+     */
+    void updatePlaneSemantics(int planeId, int clsId, double confidence);
 
-        // Running the thread
-        void Run();
-    };
-}
+    // Running the thread
+    void Run();
+};
+} // namespace ORB_SLAM3
 
 #endif // SEMANTICSEG_H
--- core/include/SemanticsManager.h
+++ core/include/SemanticsManager.h
@@ -2,141 +2,181 @@
  * This file is part of Visual S-Graphs (vS-Graphs).
  * Copyright (C) 2023-2025 SnT, University of Luxembourg
  *
- * 📝 Authors: Ali Tourani, Saad Ejaz, Hriday Bavle, Jose Luis Sanchez-Lopez, and Holger Voos
+ * 📝 Authors: Ali Tourani, Saad Ejaz, Hriday Bavle, Jose Luis Sanchez-Lopez,
+ * and Holger Voos
  *
- * vS-Graphs is free software: you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * vS-Graphs is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details: https://www.gnu.org/licenses/
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details: https://www.gnu.org/licenses/
  */
 
 #ifndef SEMANTICSMANAGER_H
 #define SEMANTICSMANAGER_H
 
 #include "Atlas.h"
-#include "Utils.h"
 #include "GeoSemHelpers.h"
+#include "Utils.h"
 
-#include <unordered_map>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl/common/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <unordered_map>
 
 namespace ORB_SLAM3
 {
-    class Atlas;
+class Atlas;
 
-    class SemanticsManager
-    {
-    private:
-        bool mGeoRuns;
-        Atlas *mpAtlas;
-        std::mutex mMutexNewRooms;
-        Eigen::Matrix4f mPlanePoseMat; // The transformation matrix from ground plane to horizontal
-        const uint8_t runInterval = 3; // The main Run() function runs every runInterval seconds
+class SemanticsManager
+{
+  private:
+    bool            mGeoRuns;
+    Atlas          *mpAtlas;
+    std::mutex      mMutexNewRooms;
+    Eigen::Matrix4f mPlanePoseMat; // The transformation matrix from ground
+                                   // plane to horizontal
+    const uint8_t   runInterval =
+        3; // The main Run() function runs every runInterval seconds
 
-        // System parameters
-        SystemParams *sysParams;
+    // System parameters
+    SystemParams *sysParams;
 
-    public:
-        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-        SemanticsManager(Atlas *pAtlas);
+  public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    SemanticsManager(Atlas *pAtlas);
 
-        /**
-         * @brief Gets the latest skeleton cluster acquired from voxblox
-         */
-        std::vector<std::vector<Eigen::Vector3d>> getLatestSkeletonCluster();
+    /**
+     * @brief       Gets the latest skeleton cluster acquired from voxblox
+     */
+    std::vector<std::vector<Eigen::Vector3d>> getLatestSkeletonCluster();
 
-        /**
-         * @brief Gets the latest detected room candidates from GNN-based room detection
-         */
-        std::vector<ORB_SLAM3::Room *> getLatestGNNRoomCandidates();
+    /**
+     * @brief       Gets the latest detected room candidates from GNN-based room
+     *              detection
+     */
+    std::vector<ORB_SLAM3::Room *> getLatestGNNRoomCandidates();
 
-        /**
-         * @brief Filters the wall planes to remove heavily tilted walls
-         */
-        void filterWallPlanes();
+    /**
+     * @brief       Filters the wall planes to remove heavily tilted walls
+     */
+    void filterWallPlanes();
 
-        /**
-         * @brief Filters the ground plane to remove points that are too far from the plane
-         * @param groundPlane the main ground plane that is the reference
-         */
-        void filterGroundPlanes(Plane *groundPlane);
+    /**
+     * @brief       Filters the ground plane to remove points that are too far
+     *              from the plane
+     *
+     * @param       groundPlane
+     *              The main ground plane that is the reference
+     */
+    void filterGroundPlanes(Plane *groundPlane);
 
-        /**
-         * @brief Transforms the plane equation to the ground reference defined by mPlanePoseMat
-         * @param planeEq the plane equation
-         * @return the transformed plane equation
-         */
-        Eigen::Vector3f transformPlaneEqToGroundReference(const Eigen::Vector4d &planeEq);
+    /**
+     * @brief       Transforms the plane equation to the ground reference
+     *              defined by mPlanePoseMat
+     *
+     * @param       planeEq
+     *              The plane equation
+     *
+     * @return      The transformed plane equation
+     */
+    Eigen::Vector3f
+        transformPlaneEqToGroundReference(const Eigen::Vector4d &planeEq);
 
-        /**
-         * @brief Gets the median height of a ground plane after transformation to referece by mPlanePoseMat
-         * @param groundPlane the ground plane
-         * @return the median height of the ground plane
-         */
-        float computeGroundPlaneHeight(Plane *groundPlane);
+    /**
+     * @brief       Gets the median height of a ground plane after
+     *              transformation to referece by mPlanePoseMat
+     *
+     * @param       groundPlane
+     *              The ground plane
+     *
+     * @return      The median height of the ground plane
+     */
+    float computeGroundPlaneHeight(Plane *groundPlane);
 
-        /**
-         * @brief Computes the transformation matrix from the ground plane to the horizontal (y-inverted)
-         * @param plane the plane
-         * @return the transformation matrix
-         */
-        Eigen::Matrix4f computePlaneToHorizontal(const Plane *plane);
+    /**
+     * @brief       Computes the transformation matrix from the ground plane to
+     *              the horizontal (y-inverted)
+     *
+     * @param       plane
+     *              The plane
+     *
+     * @return      the transformation matrix
+     */
+    Eigen::Matrix4f computePlaneToHorizontal(const Plane *plane);
 
-        /**
-         * @brief Gets the only rectangular room from the facing walls list (if exists, returns true)
-         * @param givenRoom the address of the given room
-         * @param facingWalls the facing walls list
-         * @param perpThreshDeg the perpendicular threshold in degrees
-         */
-        bool getRectangularRoom(std::pair<std::pair<Plane *, Plane *>, std::pair<Plane *, Plane *>> &givenRoom,
-                                const std::vector<std::pair<Plane *, Plane *>> &facingWalls,
-                                double perpThreshDeg = 5.0);
+    /**
+     * @brief       Gets the only rectangular room from the facing walls list
+     *              (if exists, returns true)
+     *
+     * @param       givenRoom
+     *              The address of the given room
+     *
+     * @param       facingWalls
+     *              The facing walls list
+     *
+     * @param       perpThreshDeg
+     *              The perpendicular threshold in degrees
+     */
+    bool getRectangularRoom(
+        std::pair<std::pair<Plane *, Plane *>, std::pair<Plane *, Plane *>>
+                                                       &givenRoom,
+        const std::vector<std::pair<Plane *, Plane *>> &facingWalls,
+        double                                          perpThreshDeg = 5.0);
 
-        /**
-         * @brief Checks for the existing of a room with particular walls close to a cluster.
-         * It returns the address of the existing room if found, otherwise returns nullptr.
-         * @param clusterCentroid the centroid of the cluster
-         * @param wallList the list of walls to be checked
-         */
-        ORB_SLAM3::Room *associateRooms(const Eigen::Vector3d clusterCentroid,
-            const std::vector<ORB_SLAM3::Plane *> &wallList);
-        
-        /**
-         * @brief Re-associates rooms based on fixed time intervals to avoid duplicates
-         */
-        void reAssociateRooms();
+    /**
+     * @brief       Checks for the existing of a room with particular walls
+     *              close to a cluster. It returns the address of the existing
+     *              room if found, otherwise returns nullptr.
+     *
+     * @param       clusterCentroid
+     *              The centroid of the cluster
+     *
+     * @param       wallList
+     *              The list of walls to be checked
+     */
+    ORB_SLAM3::Room *
+        associateRooms(const Eigen::Vector3d                  clusterCentroid,
+                       const std::vector<ORB_SLAM3::Plane *> &wallList);
 
-        /**
-         * @brief Converts mapped room candidates to rooms using geometric constraints
-         * 🚧 [vS-Graphs v.1.2.0] This solution is not very reliable. It is recommended to use other
-         * structural element recognition solutions.
-         */
-        void updateMapRoomCandidateToRoomGeo(ORB_SLAM3::KeyFrame *pKF);
+    /**
+     * @brief       Re-associates rooms based on fixed time intervals to avoid
+     *              duplicates
+     */
+    void reAssociateRooms();
 
-        /**
-         * @brief Processes the latest skeleton cluster to detect rooms based on free space clustering
-         */
-        void detectRoom_FreeSpaceCluster();
+    /**
+     * @brief       Converts mapped room candidates to rooms using geometric
+     *              constraints 🚧 [vS-Graphs v.1.2.0] This solution is not very
+     *              reliable. It is recommended to use other structural element
+     *              recognition solutions.
+     */
+    void updateMapRoomCandidateToRoomGeo(ORB_SLAM3::KeyFrame *pKF);
 
-        /**
-         * @brief Gets the rooms detected by the GNN module
-         */
-        void detectRoom_GNN();
+    /**
+     * @brief       Processes the latest skeleton cluster to detect rooms based
+     *              on free space clustering
+     */
+    void detectRoom_FreeSpaceCluster();
 
-        /**
-         * @brief Gets the updated floors containing rooms and corridors
-         */
-        void getUpdatedFloors();
+    /**
+     * @brief       Gets the rooms detected by the GNN module
+     */
+    void detectRoom_GNN();
 
-        // Running the thread
-        void Run();
-    };
-}
+    /**
+     * @brief       Gets the updated floors containing rooms and corridors
+     */
+    void getUpdatedFloors();
+
+    // Running the thread
+    void Run();
+};
+} // namespace ORB_SLAM3
 
 #endif // SEMANTICSEG_H
